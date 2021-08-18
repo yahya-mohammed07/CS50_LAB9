b5c5cbf553e2b131ca92fc8b1bbd9b28cade3aa3
yahya-mohammed07 <goldroger300@gmail.com>
2021-08-18 16:38:50 +0300

    SQLITE3 INSTEAD CS50 SQLITE

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from cs50 import SQL
 from flask import Flask, redirect, render_template, request
+import sqlite3 as sql
 
 # Configure application
 app = Flask(__name__)
@@ -7,8 +7,11 @@ app = Flask(__name__)
 # Ensure templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
-# Configure CS50 Library to use SQLite database
-db = SQL("sqlite:///birthdays.db")
+# connect to database
+connect = sql.connect("birthdays.db", check_same_thread=False)
+
+# get database
+db = connect.cursor()
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -21,21 +24,29 @@ def index():
     day = request.form.get("day")
 
     # insert into database
-    db.execute("INSERT INTO birthdays(name, month, day) VALUES (?,?,?)", name, month, day)
-
+    db.execute("INSERT INTO birthdays(name, month, day) VALUES (?,?,?)", (name, month, day) )
+    connect.commit()
     # go back to / and see new results
     return redirect("/")
 
   else:
     res = db.execute("SELECT * FROM birthdays")
-    return render_template("index.html", res=res)
+    col = [desc[0] for desc in db.description]
+    results = []
+
+    # making a list of dicts
+    for row in res:
+      summary = dict(zip(col, row))
+      results.append(summary)
+
+    return render_template("index.html", res=results)
 
 @app.route('/remove', methods=["GET","POST"])
 def remove():
 
   if request.method == "POST":
     id = request.form.get("id")
-    db.execute("DELETE FROM birthdays where id=?", id)
+    db.execute("DELETE FROM birthdays where id=?", (id) )
     return redirect("/")
 
   return redirect("/")
